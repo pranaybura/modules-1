55184492e2c26e384baa56f722cc5ab0d47339e7
Cheil NZ <cheilnz@Cheils-MacBook-Pro.local>
2023-01-12 14:15:37 +1300

    more of ESmodules
    using default in ESmodules

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 // import { mod1Function1 as func1, mod1Function2 as func2 } from './mod1.js' OR
-import * as modes from './mod1.js' // Here it all can be imported as an 'object' called modes.
+// import * as modes from './mod1.js' // Here it all can be imported as an 'object' called modes.
 
+// import mod1Function1, { mod1Function2 } from "./mod1.js" OR
+import lalala, { mod1Function2 } from "./mod1.js"
 const testFunction = () => {
     console.log('Im the main function')
     // func1()
-    modes.mod1Function1()
+    // mod1Function1()
+
+    lalala()
     // func2()
-    modes.mod1Function2()
+    mod1Function2()
 }
 
 testFunction()
--- mod1.js
+++ mod1.js
@@ -1,4 +1,5 @@
 const mod1Function1 = () => console.log('Mod1 is alive!')
 const mod1Function2 = () => console.log('Mod1 is rolling, baby!')
 
-export { mod1Function1, mod1Function2 }
+export default mod1Function1
+export { mod1Function2 }
